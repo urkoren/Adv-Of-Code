a14a8e8bdfc6954b0558437f3029eeef53fbbd82
zxzxxxx <ork14790@gmail.com>
2023-02-11 16:07:29 +0200

    updated day_3 solution, it is not yet complete though

--- 2022_solutions/zxzxxxx/python/day_3/solution_2.py
+++ 2022_solutions/zxzxxxx/python/day_3/solution_2.py
@@ -5,6 +5,7 @@ input_file      = "input.txt"
 three_lines     = 0
 prime_match     = " "
 
+
 def split_string(line):
     global match_letters
     len_str = len(line)
@@ -13,25 +14,13 @@ def split_string(line):
     str2 = line[len_str//2:]
     a=list(set(str1)&set(str2))
     for i in a:
-        print(i)
         match_letters.append(i)
-        print(match_letters)
-        
-def check_letter():
-    global match_letters, prime_match
-    for i in match_letters:
-        count = match_letters.count(i)
-        if count > 1:
-            prime_match = i
-            print(prime_match)
-            
        
-def swap_match_letters():
+        
+def check_on_other():
     global prime_match, match_letters
-    print(match_letters)
-    print(prime_match)
-    match_letters = []
-    match_letters.append(prime_match)
+    match_letters = prime_match
+    
     
 def check_upper_lower_case(character): 
     if character.isupper():
@@ -43,35 +32,34 @@ def convert_numbers():
     global output, match_letters
     for character in match_letters:
         if check_upper_lower_case(character):
-            print(match_letters)
             number = ord(character.lower()) - 96
             number += 26
             output += number
         else:
             number = ord(character) - 96
             output += number
-      
+    
+        
 def sulution():
     global input_file, match_letters
     with open(input_file, "r") as file:
         for line in file:
             split_string(line)
             convert_numbers()
-            match_letters   = []  
+            match_letters   = []
+            
             
 def sulution_2():
-    global input_file, three_lines, match_letters
+    global input_file, three_lines
     with open(input_file, "r") as file:
         for line in file:
             split_string(line)
             three_lines += 1
-            print (three_lines)
             if three_lines >= 3:
                 three_lines = 0
-                check_letter()
-                swap_match_letters()
+                check_on_other()
                 convert_numbers()
-                match_letters   = []
+    match_letters   = []
             
 # sulution()
 sulution_2()
